551667f96c9aa3235e0f8ee1d53014c41239954d
MAMOUN-kamal-alshisani <mamoun.alshishani@yahoo.com>
2021-04-25 13:14:35 +0300

    lab01-201

---
+++ app.js
@@ -0,0 +1,9 @@
+let clas='lab01\201';
+console.log(clas);
+
+let yourname=prompt('what is your name?');
+let age=prompt(' how old are you?');
+let live=prompt(' where do you live?');
+let color=prompt(' what is your favorite color?');
+
+alert('hello' + yourname +'your age is' + age +'you live in' + live +'your fav color is' +color);
